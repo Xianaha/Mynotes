e787e6176c8cbb64f5abff076ab7664a0f015801
Xian <1695165155@qq.com>
2024-12-23 17:10:25 +0800

    放个新脚本

---
+++ Notes/Script/n' in sentence of thesis.py
@@ -0,0 +1,18 @@
+import pyperclip
+
+# 这个脚本是用来把pdf中论文复制下来的句子中的换行符去掉，然后复制到剪贴板中。
+
+lines = []
+print("请输入文本，输入两次回车键结束：")
+while True:
+    line = input()
+    if not line:
+        break
+    lines.append(line)
+    
+text = ' '.join(lines)
+print(text)
+
+pyperclip.copy(text)
+print("已复制到剪贴板")
+
